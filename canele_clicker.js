26db795b338aa9f2cbd3655f4856f19f1c6e2f74
Sprk3lzZ <yanis.zge@gmail.com>
2020-11-07 19:43:26 +0100

    more_levels

--- canele_clicker.js
+++ canele_clicker.js
@@ -14,20 +14,32 @@ function upgrade(){
       case 1:
       if (x>=25){
          lv++;
-         x-=25;   document.getElementById("zero").innerHTML= x; document.getElementById("prix").innerHTML= 50;     
+         x-=25;   document.getElementById("zero").innerHTML= x; document.getElementById("prix").innerHTML= 100;     
       }
       break;
       case 2:
-      if (x>=50){
+      if (x>=100){
          lv=4;
-         x-=50;   document.getElementById("zero").innerHTML= x;   document.getElementById("prix").innerHTML= 100;
+         x-=100;   document.getElementById("zero").innerHTML= x;   document.getElementById("prix").innerHTML= 500;
        
    }
    break;
       case 4:
-      if (x>=100){
-         lv=8;
-         x-=100;   document.getElementById("zero").innerHTML= x ; document.getElementById("prix").innerHTML= "max";
+      if (x>=500){
+         lv=50;
+         x-=500;   document.getElementById("zero").innerHTML= x ; document.getElementById("prix").innerHTML= 5000;
+}
+break;
+      case 50:
+      if (x>=5000){
+         lv=200;
+         x-=5000;   document.getElementById("zero").innerHTML= x ; document.getElementById("prix").innerHTML= 100000;
+}
+break;
+      case 200:
+      if (x>=100000){
+         lv=500;
+         x-=100000;   document.getElementById("zero").innerHTML= x ; document.getElementById("prix").innerHTML= "max";
 }
 break;
 }
