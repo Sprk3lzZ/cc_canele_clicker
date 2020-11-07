9ee8d51a0f00a67fe6a25b9961285c68c796bbbc
Sprk3lzZ <yanis.zge@gmail.com>
2020-11-07 17:05:45 +0100

    Canelé_Clicker_D
    Canelé Clicker

---
+++ canele_clicker.js
@@ -0,0 +1,34 @@
+var x=0;
+var lv=1;
+function oncl(){
+   x+=lv;
+   textUpdate();
+    
+}
+function textUpdate(){
+    document.getElementById("zero").innerHTML=x+"";
+    
+};
+function upgrade(){
+   switch (lv) {
+      case 1:
+      if (x>=25){
+         lv++;
+         x-=25;   document.getElementById("zero").innerHTML= "";
+      }
+      break;
+      case 2:
+      if (x>=50){
+         lv=4;
+         x-=50;   document.getElementById("zero").innerHTML= "";
+       
+   }
+   break;
+      case 4:
+      if (x>=100){
+         lv=8;
+         x-=100;   document.getElementById("zero").innerHTML= "";
+}
+break;
+}
+}
